982e225ab96b05252d8ceed148de61078aaa45d5
Michal Staniaszek <m.staniaszek@gmail.com>
2018-08-21 15:24:20 +0200

    Add switches to prevent error with __builtin_ia32_monitorx
    /usr/lib/gcc/x86_64-linux-gnu/5/include/mwaitxintrin.h(36): error: identifier "__builtin_ia32_monitorx" is undefined

--- lib/make.sh
+++ lib/make.sh
@@ -8,7 +8,8 @@ CUDA_PATH=/usr/local/cuda
 cd triplet_loss
 
 nvcc -std=c++11 -c -o triplet_loss_op.cu.o triplet_loss_op_gpu.cu.cc \
-	-I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_50
+     -I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_50 \
+     -D_MWAITXINTRIN_H_INCLUDED
 
 g++ -std=c++11 -shared -o triplet_loss.so triplet_loss_op.cc \
 	triplet_loss_op.cu.o -I $TF_INC -I$TF_INC/external/nsync/public -fPIC -lcudart -L $CUDA_PATH/lib64 -L$TF_LIB -ltensorflow_framework
@@ -18,7 +19,8 @@ echo 'build triplet loss'
 cd lifted_structured_loss
 
 nvcc -std=c++11 -c -o lifted_structured_loss_op.cu.o lifted_structured_loss_op_gpu.cu.cc \
-	-I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_50
+     -I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_50 \
+     -D_MWAITXINTRIN_H_INCLUDED
 
 g++ -std=c++11 -shared -o lifted_structured_loss.so lifted_structured_loss_op.cc \
 	lifted_structured_loss_op.cu.o -I $TF_INC -I$TF_INC/external/nsync/public -fPIC -lcudart -L $CUDA_PATH/lib64 -L$TF_LIB -ltensorflow_framework
@@ -28,7 +30,8 @@ echo 'build lifted structured loss'
 cd computing_flow_layer
 
 nvcc -std=c++11 -c -o computing_flow_op.cu.o computing_flow_op_gpu.cu.cc \
-	-I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_50
+     -I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_50 \
+     -D_MWAITXINTRIN_H_INCLUDED
 
 g++ -std=c++11 -shared -o computing_flow.so computing_flow_op.cc \
 	computing_flow_op.cu.o -I $TF_INC -I$TF_INC/external/nsync/public -fPIC -lcudart -L $CUDA_PATH/lib64 -L$TF_LIB -ltensorflow_framework
@@ -38,7 +41,8 @@ echo 'build computing flow layer'
 cd backprojecting_layer
 
 nvcc -std=c++11 -c -o backprojecting_op.cu.o backprojecting_op_gpu.cu.cc \
-	-I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_50
+     -I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_50 \
+     -D_MWAITXINTRIN_H_INCLUDED
 
 g++ -std=c++11 -shared -o backprojecting.so backprojecting_op.cc \
 	backprojecting_op.cu.o -I $TF_INC -I$TF_INC/external/nsync/public -fPIC -lcudart -L $CUDA_PATH/lib64 -L$TF_LIB -ltensorflow_framework
@@ -48,7 +52,8 @@ echo 'build backprojecting layer'
 cd projecting_layer
 
 nvcc -std=c++11 -c -o projecting_op.cu.o projecting_op_gpu.cu.cc \
-	-I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_50
+     -I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_50 \
+     -D_MWAITXINTRIN_H_INCLUDED
 
 g++ -std=c++11 -shared -o projecting.so projecting_op.cc \
 	projecting_op.cu.o -I $TF_INC -I$TF_INC/external/nsync/public -fPIC -lcudart -L $CUDA_PATH/lib64 -L$TF_LIB -ltensorflow_framework
@@ -58,7 +63,8 @@ echo 'build projecting layer'
 cd computing_label_layer
 
 nvcc -std=c++11 -c -o computing_label_op.cu.o computing_label_op_gpu.cu.cc \
-	-I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_50
+     -I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_50 \
+     -D_MWAITXINTRIN_H_INCLUDED
 
 g++ -std=c++11 -shared -o computing_label.so computing_label_op.cc \
 	computing_label_op.cu.o -I $TF_INC -I$TF_INC/external/nsync/public -fPIC -lcudart -L $CUDA_PATH/lib64 -L$TF_LIB -ltensorflow_framework
